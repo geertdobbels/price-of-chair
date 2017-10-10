4a7424fcfcce5b30de363e99b24f6069ccb32f1b
Geert Dobbels <dobbels.geert@gmail.com>
2017-10-10 22:31:26 +0200

    modifications necessary for heroku deployment

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,10 @@
-__author__ = 'jslvtr'
+__author__ = 'Geert Dobbels'
 
 
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
-ALERT_TIMEOUT = 10
+
 COLLECTION = "alerts"
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
+
+ALERT_TIMEOUT=10
