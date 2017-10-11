9af32ef3833fff37e51ff40f04527f21776b0a31
Geert Dobbels <dobbels.geert@gmail.com>
2017-10-11 12:58:03 +0200

    Fix src.models.items.item.py load_price method to accept "non-american" price formats

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -23,15 +23,27 @@ class Item(object):
         return "<Item {} with URL {}>".format(self.name, self.url)
 
     def load_price(self):
+        # Amazon: <span id="priceblock_ourprice" class="a-size-medium a-color-price">$2,499.00</span>
         request = requests.get(self.url)
         content = request.content
         soup = BeautifulSoup(content, "html.parser")
         element = soup.find(self.tag_name, self.query)
         string_price = element.text.strip()
-
-        pattern = re.compile("(\d+.\d+)")
-        match = pattern.search(string_price)
-        self.price = float(match.group())
+        string_price = string_price.replace(' ','')   # french has spaces as thousands separators, so eliminate spaces
+        pattern = re.compile("((?:(?:\d+)*[\.|\,])*\d+)")  # Select only digits and "," or "." (delete currency symbols)
+        string_price = pattern.search(string_price).group()
+        eu_pattern = re.compile("\d*\.\d*\,")
+        us_pattern = re.compile("\d*\,\d*\.")
+        if eu_pattern.search(string_price):         # if thousand separator = '.' and decimal sep. = ","
+            string_price.replace('.','')            # then delete '.' and afterwards change ',' into '.'
+            string_price.replace(',','.')
+        elif us_pattern.search(string_price):       # if thousand separator = ','
+            string_price.replace(',','')            # then delete all ','
+        string_price = string_price.replace(',','.')    # if number only contains one ',' then change it to '.'
+        try:
+            self.price = float(string_price)
+        except ItemErrors.StringToFloatError as e:
+            return e.message
 
         return self.price
 
